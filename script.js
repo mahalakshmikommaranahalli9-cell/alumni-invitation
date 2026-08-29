a9ad139b2b7e0bad66f6166f1a0e651889f9f29e
MAHALAKSHMI KOMMARANAHALLI <mahalakshmikommaranahalli9@gmail.com>
2026-08-29 16:11:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+function launchConfetti() {
+  const duration = 3 * 1000;
+  const end = Date.now() + duration;
+
+  (function frame() {
+    confetti({
+      particleCount: 5,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0 }
+    });
+    confetti({
+      particleCount: 5,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1 }
+    });
+
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  }());
+}
+
+document.querySelector(".button").addEventListener("click", launchConfetti);
